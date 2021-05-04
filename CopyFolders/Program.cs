eb9ac1f69b2ac9700c289bde2ef55f23d0252b1b
KeepD0wn <38407409+KeepD0wn@users.noreply.github.com>
2021-05-05 00:07:57 +0300

    Initial commit

---
+++ CopyFolders/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyFolders
+{
+    class Program
+    {
+        static void CopyDirectory(DirectoryInfo source, DirectoryInfo destination)
+        {
+            if (!destination.Exists)
+            {
+                destination.Create();
+            }
+
+            // Copy all files.
+            FileInfo[] files = source.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                file.CopyTo(Path.Combine(destination.FullName,
+                    file.Name),true);
+            }
+
+            // Process subdirectories.
+            DirectoryInfo[] dirs = source.GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                // Get destination directory.
+                string destinationDir = Path.Combine(destination.FullName, dir.Name);
+
+                // Call CopyDirectory() recursively.
+                CopyDirectory(dir, new DirectoryInfo(destinationDir));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // System.IO.File.Copy(@"C:\Users\gvozd\Desktop\1188453866\730", @"C:\Users\gvozd\Desktop\1158606011", true);
+            //File.Copy(@"C:\Users\gvozd\Desktop\1188453866\730", Path.Combine(@"C:\Users\gvozd\Desktop\1158606011", Path.GetFileName(@"C:\Users\gvozd\Desktop\1188453866\730")), false);
+
+            DirectoryInfo sourceDir = new DirectoryInfo(@"C:\Users\gvozd\Desktop\1188453866");
+            DirectoryInfo destinationDir = new DirectoryInfo(@"C:\Users\gvozd\Desktop\1158606011");
+
+            CopyDirectory(sourceDir, destinationDir);
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReplaceFolders/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace ReplaceFolders
+{
+    class Program
+    {
+        static void CopyDirectory(DirectoryInfo source, DirectoryInfo destination)
+        {
+            if (!destination.Exists)
+            {
+                destination.Create();
+            }
+
+            // Copy all files.
+            FileInfo[] files = source.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                file.CopyTo(Path.Combine(destination.FullName,
+                    file.Name), true);
+            }
+
+            // Process subdirectories.
+            DirectoryInfo[] dirs = source.GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                // Get destination directory.
+                string destinationDir = Path.Combine(destination.FullName, dir.Name);
+
+                // Call CopyDirectory() recursively.
+                CopyDirectory(dir, new DirectoryInfo(destinationDir));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (File.Exists($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic"))
+                {
+                    string key = "";
+                    using (StreamReader sr = new StreamReader($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic"))
+                    {
+                        key = sr.ReadToEnd();
+                    }
+                    key = key.Replace("\r\n", "");
+
+                    if (PcInfo.GetCurrentPCInfo() == key)
+                    {
+                        string mainPath = @"C:\Program Files (x86)\Steam\userdata";
+                        DirectoryInfo dir = new DirectoryInfo(mainPath);
+                        int g = 0;
+                        foreach (var item in dir.GetDirectories())
+                        {
+                            DirectoryInfo sourceDir = new DirectoryInfo($@"{AppDomain.CurrentDomain.BaseDirectory}\reference");
+                            DirectoryInfo destinationDir = new DirectoryInfo($@"{mainPath}\{item.Name}");
+                            CopyDirectory(sourceDir, destinationDir);
+
+                            g += 1;
+                            Console.WriteLine("Folders replaced: " + g);
+                        }
+                        Console.WriteLine("Done");
+                    }
+                    else
+                    {
+                        Console.WriteLine("[SYSTEM] License not found");
+                        Thread.Sleep(5000);
+                        Environment.Exit(0);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("[SYSTEM] License not found");
+                    Thread.Sleep(5000);
+                    Environment.Exit(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }            
+            Console.ReadLine();
+        }
+    }
+}
