cd299fdd97257af7e348cc092396f8fa16bf5e29
KeepD0wn <38407409+KeepD0wn@users.noreply.github.com>
2023-06-10 01:00:25 +0300

    udp 10.06.2023
    - перенесён в проект TFCaser

--- ReplaceFolders/Program.cs
+++ ReplaceFolders/Program.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Threading;
-using MySql.Data.MySqlClient;
-using System.Configuration;
-using System.Data.Common;
+using GeneralDLL;
 
 namespace ReplaceFolders
 {
@@ -55,99 +49,53 @@ namespace ReplaceFolders
             }
         }
 
-        private static void CheckSubscribe(string key)
-        {
-            MySqlConnection conn = new MySqlConnection();
-            try
-            {
-                conn = new MySqlConnection(Properties.Resources.String1);
-                conn.Open();
-
-                var com = new MySqlCommand("USE subs; " +
-                 "select * from `subs` where keyLic = @keyLic AND subEnd > NOW() AND activeLic = 1 limit 1", conn);
-                com.Parameters.AddWithValue("@keyLic", key);
-
-                using (DbDataReader reader = com.ExecuteReader())
-                {
-                    if (reader.HasRows) //тут уходит на else если нет данных
-                    {
-                        reader.Read();
-                        string dataEnd = reader.GetString(2);
-                        Console.WriteLine($"Subscription will end {dataEnd}");
-                        reader.Close();
-                    }
-                    else
-                    {
-                        conn.Close();
-                        Console.WriteLine("[500][SYSTEM] License is not active");
-                        Thread.Sleep(5000);
-                        Environment.Exit(0);
-                    }
-                }
-                conn.Close();
-            }
-            catch
-            {
-                conn.Close();
-                Console.WriteLine("[SYSTEM][404] Something went wrong!");
-                Thread.Sleep(5000);
-                Environment.Exit(0);
-            }
-            finally
-            {
-                conn.Close();
-            }
-        }
+        public static string assemblyName = "Replace Folders";
 
         static void Main(string[] args)
         {
-            Console.Title = "Replace Folders";
+            GeneralDLL.Debugger.CheckDebugger();
+            Console.Title = assemblyName;
             try
             {
-                if (File.Exists($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic")) //File.Exists($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic") 
+                if (File.Exists($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic"))
                 {
-                    string key = "";
-                    using (StreamReader sr = new StreamReader($@"{AppDomain.CurrentDomain.BaseDirectory}\License.lic"))
-                    {
-                        key = sr.ReadToEnd();
-                    }
-                    key = key.Replace("\r\n", "");                                        
+                    string key = Subscriber.GetKey();                               
 
-                    if (PcInfo.GetCurrentPCInfo() == key) //PcInfo.GetCurrentPCInfo() == key 
+                    if (PcInfo.GetCurrentPCInfo() == key) 
                     {
-                        CheckSubscribe(key);
+                        Subscriber.CheckSubscribe(key,Games.ANY);
 
                         string mainPath = @"C:\Program Files (x86)\Steam\userdata";
                         DirectoryInfo dir = new DirectoryInfo(mainPath);
-                        int g = 0;
+                        int counter = 0;
                         foreach (var item in dir.GetDirectories())
                         {
                             DirectoryInfo sourceDir = new DirectoryInfo($@"{AppDomain.CurrentDomain.BaseDirectory}\reference");
                             DirectoryInfo destinationDir = new DirectoryInfo($@"{mainPath}\{item.Name}");
                             CopyDirectory(sourceDir, destinationDir);
 
-                            g += 1;
-                            Console.WriteLine("Folders replaced: " + g);
+                            counter += 1;
+                            Console.WriteLine("Folders replaced: " + counter);
                         }
                         Console.WriteLine("Done");
                     }
                     else
                     {
-                        Console.WriteLine("[SYSTEM] License not found");
+                        Logger.LogAndWritelineAsync($"[014][{assemblyName}] License not found");
                         Thread.Sleep(5000);
                         Environment.Exit(0);
                     }
                 }
                 else
                 {
-                    Console.WriteLine("[SYSTEM] License not found");
+                    Logger.LogAndWritelineAsync($"[015][{assemblyName}] License not found");
                     Thread.Sleep(5000);
                     Environment.Exit(0);
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Logger.LogAndWritelineAsync($"[{assemblyName}] {ex.Message}");
             }            
             Console.ReadLine();
         }
